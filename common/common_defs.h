ced7e8d98ec468dda1102467331b6065634998dc
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-03-07 11:30:48 +0100

    added common header for types used by firmware and gui

---
+++ common/common_defs.h
@@ -0,0 +1,24 @@
+#ifndef COMMON_DEFS_H
+#define COMMON_DEFS_H
+
+enum class SELECTOR{
+    ZERO,
+    ONE,
+    TWO,
+    TREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    ELEVEN,
+    TWELVE,
+    THIRTEEN,
+    FOURTEEN,
+    FIFTEEN,
+    ALL,
+};
+
+#endif // COMMON_DEFS_H
